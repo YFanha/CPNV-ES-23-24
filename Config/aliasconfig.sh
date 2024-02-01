27444e691dea2d63c4de6e07121dec208eb97440
Yann Fanha <yannfnha@gmail.com>
2024-02-01 14:59:48 +0100

    new file:   Config/aliasconfig.sh

---
+++ Config/aliasconfig.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+file="/home/$USER/.bash_aliases"
+cat >> $file << EOL
+alias ll='ls -l'
+alias la='ls -la'
+alias ct='clear && tree'
+alias cta='clear && tree -a'
+alias p='cat'
+alias h='head'
+EOL
+
